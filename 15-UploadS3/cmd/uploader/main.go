07cc1506b6d63ea6cc13d97182bc61ff381388d5
Wesley <wesleywillians@gmail.com>
2022-11-15 14:34:45 -0500

    Capitulo 15

--- 15-UploadS3/cmd/uploader/main.go
+++ 15-UploadS3/cmd/uploader/main.go
@@ -80,7 +80,11 @@ func uploadFile(filename string, uploadControl <-chan struct{}, errorFileUpload 
 	if err != nil {
 		fmt.Printf("Error opening file %s\n", completeFileName)
 		<-uploadControl // esvazia o canal
+<<<<<<< HEAD
 		errorFileUpload <- completeFileName
+=======
+		errorFileUpload <- filename
+>>>>>>> d86f2c7 (Capitulo 15)
 		return
 	}
 	defer f.Close()
@@ -92,7 +96,11 @@ func uploadFile(filename string, uploadControl <-chan struct{}, errorFileUpload 
 	if err != nil {
 		fmt.Printf("Error uploading file %s\n", completeFileName)
 		<-uploadControl // esvazia o canal
+<<<<<<< HEAD
 		errorFileUpload <- completeFileName
+=======
+		errorFileUpload <- filename
+>>>>>>> d86f2c7 (Capitulo 15)
 		return
 	}
 	fmt.Printf("File %s uploaded successfully\n", completeFileName)
